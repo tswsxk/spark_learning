0aef7b1b53b6317ae159cfa4a644a2f81eb822f6
shiweitong <shiweitong@tencent.com>
2024-04-12 17:35:35 +0800

    [feat] initialization

---
+++ README.md
@@ -0,0 +1,18 @@
+# Beginner Tutorial
+
+## Preparation
+
+1. Install dependencies and initial the package
+
+```bash
+pip install -e .
+```
+
+2. Run the demo script to see whether everything has been prepared
+
+```bash
+cd scripts/beginner
+python beginner.py
+```
+
+If you see `*** Spark ***` in the terminal, then everything goes well.
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+test_deps = [
+    'pytest>=4',
+    'pytest-cov>=2.6.0',
+    'pytest-flake8<5.0.0',
+    'flake8<5.0.0'
+]
+
+spider_deps = [
+    "google-cloud-bigquery",
+    "selenium",
+    ""
+]
+
+setup(
+    name='spark_learning',
+    version='0.0.1',
+    extras_require={
+        'test': test_deps,
+        'spider': spider_deps
+    },
+    install_requires=[
+        "pandas",
+        "scikit-learn",
+        "dill",
+        "tqdm",
+        "loguru",
+        "lightgbm"
+    ],  # And any other dependencies foo needs
+    entry_points={
+    },
+)
